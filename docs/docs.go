d0223abed59c0d731f7239f6911b9a6a1b9cbaef
Zhannur Diyas <zhdiyas@dar.io>
2024-09-26 09:04:40 +0500

    fix: swag delimiters

--- docs/docs.go
+++ docs/docs.go
@@ -138,8 +138,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
 }
 
 func init() {
